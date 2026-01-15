7ed379abc8d8c98d25ace9d388536c8bab4a3616
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-15 17:07:50 -0300

    Update components

--- src/Process.php
+++ src/Process.php
@@ -45,17 +45,9 @@ class Process
 
   public function router(
   ) {
-    file_put_contents("config.json", Util::sprintFormat( 
-        "php -S localhost:8080 -t %s", [
-          Util::path( [ 
-            dirname(__FILE__), "Starteds", "Router.php" 
-          ])
-        ]
-      )
-    );
     $this->createRun(
       Util::sprintFormat( 
-        "php -S localhost:8080 -t %s", [
+        "php -S localhost:8080 %s", [
           Util::path( [ 
             dirname(__FILE__), "Starteds", "Router.php" 
           ])
