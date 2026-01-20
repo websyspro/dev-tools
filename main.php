876c680f2b997b343514341d1d624095d5cc9528
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-20 18:46:14 -0300

    Update components

--- main.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Websyspro\DevTools\Shareds\WebSocket;
-
-$ws = new WebSocket(3000);
--- main_.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once dirname( __FILE__ ) . "/vendor/autoload.php";
-
-use Websyspro\DevTools\Shareds\WebSocketNotifier;
-
-$wsn = new WebSocketNotifier();
-$wsn->notify( "reload" );
